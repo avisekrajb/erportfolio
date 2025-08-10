ef122bf3fc0bfd1fb1d516fcb74ef44b8b2434b2
avisekrajb <144264226+avisekrajb@users.noreply.github.com>
2025-08-10 11:45:09 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -68,5 +68,6 @@ app.get('*', (req, res) => {
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-}
+    console.log(`Server running on port ${PORT}`);
+  });
+  
